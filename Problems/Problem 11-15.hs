af6b367bc20e459ba296d922d596c3a9256ce24c
Tobias Savinainen <tcs1000@hotmail.com>
2012-10-10 16:38:16 +0200

    Added descriptions for problems 11-15

---
+++ Problems/Problem 11-15.hs
@@ -0,0 +1,43 @@
+--------------------------------------------------
+-- Problem 11
+-- 
+-- Modified run-length encoding.
+-- Modify the result of problem 10 in such a way 
+-- that if an element has no duplicates it is 
+-- simply copied into the result list. Only 
+-- elements with duplicates are transferred as 
+-- (N E) lists.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 12
+-- 
+-- Decode a run-length encoded list.
+--
+-- Given a run-length code list generated as 
+-- specified in problem 11. Construct its 
+-- uncompressed version.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 13
+-- 
+-- Run-length encoding of a list (direct solution).
+-- 
+-- Implement the so-called run-length encoding 
+-- data compression method directly. I.e. don't 
+-- explicitly create the sublists containing the 
+-- duplicates, as in problem 9, but only count 
+-- them. As in problem P11, simplify the result 
+-- list by replacing the singleton lists (1 X) 
+-- by X.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 14
+--
+-- Duplicate the elements of a list
+--------------------------------------------------

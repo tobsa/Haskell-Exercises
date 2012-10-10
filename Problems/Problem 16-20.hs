416ccc42553d1644dfcffc87fbf19156b9154fb0
Tobias Savinainen <tcs1000@hotmail.com>
2012-10-10 18:53:03 +0200

    Added descriptions for problem 16-20

---
+++ Problems/Problem 16-20.hs
@@ -0,0 +1,48 @@
+--------------------------------------------------
+-- Problem 16
+-- 
+-- Drop every N'th element from a list.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 17
+-- 
+-- Split a list into two parts; the length of the first part is given.
+--
+-- Do not use any predefined predicates.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 18
+-- 
+--  Extract a slice from a list.
+--
+-- Given two indices, i and k, the slice is the 
+-- list containing the elements between the i'th 
+-- and k'th element of the original list (both 
+-- limits included). Start counting the elements
+-- with 1.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 19
+-- 
+-- Rotate a list N places to the left.
+--
+-- Hint: Use the predefined functions length and 
+-- (++).
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 20
+-- 
+-- Rotate a list N places to the left.
+--
+-- Remove the K'th element from a list.
+--------------------------------------------------
+-- Not solved --
+

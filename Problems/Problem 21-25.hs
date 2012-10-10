75d2636740dfc58cf4db68fe1177379ccb4e0e96
Tobias Savinainen <tcs1000@hotmail.com>
2012-10-11 00:18:40 +0200

    Added descriptions for problems 21-25

---
+++ Problems/Problem 21-25.hs
@@ -0,0 +1,39 @@
+--------------------------------------------------
+-- Problem 21
+--
+-- Insert an element at a given position into a 
+-- list.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 22
+--
+-- Create a list containing all integers within a 
+-- given range.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 23
+--
+-- Extract a given number of randomly selected 
+-- elements from a list.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 24
+--
+-- Lotto: Draw N different random numbers from 
+-- the set 1..M.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 25
+--
+-- Generate a random permutation of the elements 
+-- of a list.
+--------------------------------------------------
+-- Not solved --

c6899092ad20ca62475acd0c3d25e1c684c1756f
Tobias Savinainen <tcs1000@hotmail.com>
2012-10-10 16:37:36 +0200

    Added problem description

--- Problems/Problem 6-10.hs
+++ Problems/Problem 6-10.hs
@@ -41,5 +41,14 @@ pack' :: (Eq a) => [a] -> [[a]]
 pack' []     = []
 pack' (x:xs) = (x : takeWhile (x==) xs) : pack' (dropWhile (x==) xs) 
 
-
-
+--------------------------------------------------
+-- Problem 10
+-- 
+-- Run-length encoding of a list. Use the result 
+-- of problem P09 to implement the so-called 
+-- run-length encoding data compression method. 
+-- Consecutive duplicates of elements are encoded 
+-- as lists (N E) where N is the number of 
+-- duplicates of the element E.
+--------------------------------------------------
+-- Not Solved --

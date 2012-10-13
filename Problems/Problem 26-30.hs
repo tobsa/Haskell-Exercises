422cbfe9128023dabaa0a9910d11a130a3edb0c1
Tobias Savinainen <tcs1000@hotmail.com>
2012-10-13 19:44:57 +0200

    Added descriptions for problem 26-30

---
+++ Problems/Problem 26-30.hs
@@ -0,0 +1,24 @@
+--------------------------------------------------
+-- Problem 26
+--
+-- Generate the combinations of K distinct objects 
+-- chosen from the N elements of a list
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 27
+--
+-- Group the elements of a set into disjoint 
+-- subsets.
+--------------------------------------------------
+-- Not solved --
+
+--------------------------------------------------
+-- Problem 28
+--
+-- Sorting a list of lists according to length of 
+-- sublists
+--------------------------------------------------
+-- Not solved --
+
